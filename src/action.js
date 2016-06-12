464b6bd9d70881dbda4fbb9021344066106af746
roccosportal <tadosntmas@riseup.net>
2016-06-12 16:56:31 +0200

    added message: command not found

--- src/action.js
+++ src/action.js
@@ -4,7 +4,11 @@ webcli.action = {
     var response = function(success, message, info){
        callback({ command : command, success : success, message : message, info : info});
     };
-    webcli.action.doEvent.call(command.command, {ui : ui, args: command.args}, response);
+    var unhandled = function(){
+       // no one handled this event
+       response(false, 'Command not found', {command : command.command});
+    };
+    webcli.action.doEvent.call(command.command, {ui : ui, args: command.args}, response, unhandled);
   }
 };
 webcli.action.doEvent = webcli.eventhandler.createEventHandler(null);
--- src/eventhandler.js
+++ src/eventhandler.js
@@ -5,13 +5,18 @@ webcli.eventhandler.createEventHandler = function(){
   instance.on = function(event, callback){
     instance.listeners.push({'event' : event, 'callback' : callback});
   };
-  instance.call = function (event, options, callback){
+  instance.call = function (event, options, callback, unhandledCallback){
     for (var i = 0; i < instance.listeners.length; i++) {
       if(instance.listeners[i].event == event){
          return instance.listeners[i].callback(options, callback);
       }
     }
-    return callback();
+    if(typeof unhandledCallback !== 'undefined'){
+      return unhandledCallback();
+    }
+    else {
+      return callback();
+    }
   };
   instance.remove = function(event, callback){
     for (var i = 0; i < instance.listeners.length; i++) {
